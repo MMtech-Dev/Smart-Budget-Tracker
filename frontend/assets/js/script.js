f55c1b712bb8117e90817d507167477f12ff4787
Nsengimana François <francknsengimana@gmail.com>
2025-02-08 11:48:19 +0100

    Added bootstrap and environment preparation

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,13 +1,4 @@
 "use strict";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const burgerMenuButton = document.querySelector(".burger-menu-button");
-const burgerMenuButtonIcon = document.querySelector(".burger-menu-button i");
-const burgerMenu = document.querySelector(".burger-menu");
-
-burgerMenuButton.onclick = function () {
-  burgerMenu.classList.toggle("open");
-  const isOpen = burgerMenu.classList.contains("open");
-  burgerMenuButtonIcon.classList = isOpen
-    ? "fa-solid fa-xmark"
-    : "fa-solid fa-bars";
-};
